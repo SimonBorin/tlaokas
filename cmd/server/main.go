60cf9d6869856785334953fc99f6417d5eca877e
simon.borin <simon.a.borin@ringcentral.com>
2025-09-10 18:54:28 +0200

    fix: /api

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,8 +15,8 @@ func main() {
 	}
 	go cleanup.Run()
 
-	http.Handle("/secret", handler.WithCORS(http.HandlerFunc(handler.CreateSecret)))
-	http.Handle("/secret/", handler.WithCORS(http.HandlerFunc(handler.GetSecret)))
+	http.Handle("/api/secret", handler.WithCORS(http.HandlerFunc(handler.CreateSecret)))
+	http.Handle("/api/secret/", handler.WithCORS(http.HandlerFunc(handler.GetSecret)))
 
 	log.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
